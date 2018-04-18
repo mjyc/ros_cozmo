0e7ba31f7d9bdf62cfbd9a0703f6b57073bdd873
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-04-17 22:52:22 -0700

    WIP

--- src/ros_cozmo_node.py
+++ src/ros_cozmo_node.py
@@ -1,100 +1,71 @@
 #! /usr/bin/env python3
 
 import sys
-from functools import partial
 import cozmo
 import actionlib
 import rospy
 import ros_cozmo.msg
 
-# import time
-import asyncio
-
-
-class CozmoAction(object):
-
-    def __init__(self, name, coz, Action):
+class CozmoBridge(object):
+    def __init__(self, coz):
         self._robot = coz
-        self._action_name = name
+        self._servers = {}
+        self._actions = {}
 
-        self._goal = None
+    @property
+    def servers(self):
+        return self._servers
 
-        self._action = None
-
-        self._as = actionlib.SimpleActionServer(
-            self._action_name,
+    def createActionServer(self, name, Action):
+        self._servers[name] = actionlib.SimpleActionServer(
+            name,
             Action,
             auto_start=False
         )
-        self._as.register_goal_callback(self._goal_cb)
-        self._as.register_preempt_callback(self._preempt_cb)
-        self._as.start()
 
-    def _goal_cb(self):
-        rospy.loginfo("Goal accepted")
-        goal = self._as.accept_new_goal()
-        robot_action_args = dict((key, getattr(goal, key)) for key in goal.__slots__)
-        if "in_parallel" not in robot_action_args:
-            robot_action_args["in_parallel"] = True
-        robot_action_name = self._action_name.strip("/")
-        robot_action = getattr(self._robot, robot_action_name)
-        # self._action =
-        action = robot_action(**robot_action_args)
+        '''Cancels the running goal on receiving a new goal'''
+        def goal_cb():
+            if self._servers[name].is_active():
+                print("2")
+                self._actions[name].abort()
+            goal = self._servers[name].accept_new_goal()
+            args = dict((key, getattr(goal, key)) for key in goal.__slots__)
+            if "in_parallel" not in args:
+                args["in_parallel"] = True
+            robot_method = getattr(self._robot, name.strip("/"))
+            self._actions[name] = robot_method(**args)
 
-        def fx(evt, **kwargs):
-            print(evt, kwargs)
-            print("done!!")
-        action.on_completed(fx)
+            def callback(evt, **kwargs):
+                print("evt %s" % (evt))
+                print("kwargs %s" % (kwargs))
+                del self._actions[name]
+                self._servers[name].set_succeeded()
+            self._actions[name].on_completed(callback)
 
-        # self._action.on_completed()
-        # print(self._action)
+        def _preempt_cb(self):
+            print("1")
+            if self._servers[name].is_active():
+                print("2")
+                self._actions[name].abort()
+                self._servers[name].set_preempted()
 
-        # self._action.wait_for_completed()
-        # print("dooone!")
-        # time.sleep(1.5)
-        # self._as.set_succeeded()
+        self._servers[name].register_goal_callback(goal_cb)
+        # server.register_preempt_callback(self._preempt_cb)
 
-    def _preempt_cb(self):
-        rospy.loginfo("preempt called")
-        # print(self._action)
-        # print(self._as.is_active())
-        # print(self._as.is_active())
-        pass
-        # raise NotImplementedError()
-        # if self._as.is_active():
-        #     rospy.loginfo("Action preempted")
-        #     self._as.set_preempted()
-
-
-class Cozmo(object):
-    def __init__(self, coz):
-        self._robot = coz
-
-        def gcb(self):
-            goal = self._as.accept_new_goal()
-            print(goal)
-            g = goal
-            args = dict((key, getattr(g, key)) for key in g.__slots__)
-            self._robot.say_text(**args).wait_for_completed()
-            self._as.set_succeeded()
-
-        SayTextCozmoAction = type(
-            "SayTextCozmoAction",
-            (CozmoAction,),
-            {}
-        )
-        server = SayTextCozmoAction("/say_text", coz, ros_cozmo.msg.SayTextAction)
-
+        return self._servers[name]
 
 def run(coz_conn):
-    "The run method runs once Cozmo is connected."
+    '''The run method runs once Cozmo is connected.'''
     coz = coz_conn.wait_for_robot()
 
     rospy.init_node("cozmo", log_level=rospy.DEBUG)
-    print("rospy.get_name()", rospy.get_name())
-    Cozmo(coz)
-    rospy.spin()
 
+    cozmo_bridge = CozmoBridge(coz)
+    cozmo_bridge.createActionServer('/say_text', ros_cozmo.msg.SayTextAction)
+    for name, server in cozmo_bridge.servers.items():
+        server.start()
+
+    rospy.spin()
 
 if __name__ == "__main__":
     try:
